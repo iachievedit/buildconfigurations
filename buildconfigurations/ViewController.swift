35dcca68f2af176ed08eb9fce8e7b25c0373bbd3
Joe Bell <joeaintexas@gmail.com>
2014-06-14 16:49:48 -0500

    Initial Commit

---
+++ buildconfigurations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buildconfigurations
+//
+//  Created by Joseph Bell on 6/14/14.
+//  Copyright (c) 2014 iAchieved.it LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
